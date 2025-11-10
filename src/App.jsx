628c78ffc9c38c3eaea19245962d33323c19bf37
Gustavo H. K. Oliveira <123880096+gustavo-h-k-oliveira@users.noreply.github.com>
2025-11-10 17:14:19 +0000

    Atualizando imagem e aprimorando design responsivo.

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,8 @@ export default function App() {
   }, [])
   return (
       <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-  <div className="relative flex w-[120rem] h-[67.5rem] py-[3.125rem] px-[5rem] md:px-[7.5rem] flex-col items-start gap-[4.0625rem] rounded shadow-lg overflow-auto bg-[linear-gradient(180deg,_#FFF_50%,_#999_100%)] z-0">
-          <div className="absolute top-[5.69rem] w-[84.58988rem] h-[56.0625rem] flex-shrink-0 aspect-[1353.44/897.00] bg-transparent overflow-hidden left-1/2 -translate-x-1/2 xl:left-auto xl:right-[3.29rem] xl:translate-x-0">
+        <div className="relative flex w-[120rem] h-[67.5rem] py-[3.125rem] px-[5rem] md:px-[7.5rem] flex-col items-start gap-[4.0625rem] rounded shadow-lg overflow-auto bg-[linear-gradient(180deg,_#FFF_50%,_#999_100%)] z-0">
+          <div className="absolute top-[5.69rem] w-[84.58988rem] h-[56.0625rem] flex-shrink-0 aspect-[1353.44/897.00] bg-transparent overflow-hidden left-1/2 -translate-x-1/2 2xl:left-auto 2xl:right-[3.29rem] 2xl:translate-x-0">
             <img
               src={GranTurismoLogo}
               alt="Gran Turismo Logo"
@@ -80,16 +80,16 @@ export default function App() {
             </div>
           </header>
 
-          <main className="flex-1 w-full rounded p-[0.625rem] flex justify-between items-start h-[49.8125rem] flex-shrink-0 self-stretch">
-            <div className="flex flex-col items-start gap-[0.5rem] relative z-10">
-              <h1 className="[font-family:'Instrument Sans'] text-[9.375rem] text-[var(--gray800,#353535)] font-bold leading-normal">
+          <main className="flex-1 w-full rounded p-[0.625rem] flex flex-col items-center gap-[3rem] 2xl:flex-row 2xl:justify-between xl:items-start xl:gap-0 h-[49.8125rem] flex-shrink-0 self-stretch">
+            <div className="flex flex-col items-center gap-[0.5rem] relative z-10 xl:items-start xl:text-left">
+              <h1 className="[font-family:'Instrument Sans'] text-[9.375rem] text-[var(--gray800,#353535)] font-bold leading-none">
                 Car Culture
               </h1>
-              <p className="w-[51.5rem] text-[var(--black,#000)] [font-family:'Instrument Sans'] text-[5rem] font-semibold leading-tight">
+              <p className="w-full 2xl:w-[51.5rem] text-center xl:text-left text-[var(--black,#000)] [font-family:'Instrument Sans'] text-[5rem] font-semibold leading-tight">
                 More than passion for machines, it’s a <span className="text-sky-600">lifestyle</span>
               </p>
             </div>
-            <div className="flex flex-col justify-end items-end gap-[3.75rem] py-[3.75rem] self-stretch relative z-10">
+            <div className="flex flex-row flex-wrap justify-center items-center gap-[3.75rem] mx-[5rem] xl:py-[3.75rem] self-stretch relative z-20 bg-white/50 xl:bg-transparent backdrop-blur-sm xl:backdrop-blur-none rounded-lg p-4 2xl:ml-0 2xl:flex-col xl:justify-end 2xl:items-end 2xl:flex-nowrap">
               <LinkedImage href="https://github.com/gustavo-h-k-oliveira" src={GitHub} alt="GitHub logo" size="lg" iconOnly ariaLabel="GitHub" />
               <LinkedImage href="https://instagram.com/gustavo.h.k.oliveira" src={Instagram} alt="Instagram logo" size="lg" iconOnly ariaLabel="Instagram" />
               <LinkedImage href="https://linkedin.com/in/gustavo-oliveira-713583214" src={LinkedIn} alt="LinkedIn logo" size="lg" iconOnly ariaLabel="LinkedIn" />
@@ -97,14 +97,14 @@ export default function App() {
             </div>
           </main>
         </div>
-      <img
-        src={Car}
-        alt="Carro esportivo"
-        className="w-[80.09375rem] h-[39.4375rem] aspect-[1281.50/631.00] absolute left-1/2 -translate-x-1/2 top-[28.06rem] bg-[lightgray_0px_0px/_100%_135.341%_no-repeat] object-cover"
-      />
       <div className="absolute left-0 top-[62.12rem] flex items-center px-[5rem] md:px-[7.5rem] w-full h-[5.375rem] bg-[linear-gradient(180deg,_#949494_1.0%,_#9E9E9E_15.01%,_#CECECE_60.56%,_#FFF_98.54%)]">
         <p className='text-sm text-gray-600'>© <strong>GHK</strong> 2025</p>
       </div>
+      <img
+        src={Car}
+        alt="Carro esportivo"
+        className="w-[69.625rem] xl:h-[30.9] 2xl:h-[14.8125rem] aspect-[1114/237] absolute left-1/2 -translate-x-1/2 top-[47.7rem] bg-[lightgray_0px_0px/_100%_135.341%_no-repeat] object-cover"
+      />
     </div>
   )
 }
